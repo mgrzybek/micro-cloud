2492e06290ec755a8109745788504c7a286bf8dd
Mathieu Grzybek <github@grzybek.fr>
2025-08-09 17:28:29 +0200

    Formating scripts
    Signed-off-by: Mathieu Grzybek <github@grzybek.fr>

--- ring0/scripts/deploy-pki.sh
+++ ring0/scripts/deploy-pki.sh
@@ -9,67 +9,67 @@ RING0_ROOT="$(dirname $0)/.."
 
 source common.sh
 
-if [[ -z "$PKI_COUNTRY" ]] ; then
-    echo "PKI_COUNTRY must be defined"
-    exit 1
+if [[ -z "$PKI_COUNTRY" ]]; then
+	echo "PKI_COUNTRY must be defined"
+	exit 1
 fi
 
-if [[ -z "$PKI_LOCATION" ]] ; then
-    echo "PKI_LOCATION must be defined"
-    exit 1
+if [[ -z "$PKI_LOCATION" ]]; then
+	echo "PKI_LOCATION must be defined"
+	exit 1
 fi
 
-if [[ -z "$PKI_ORG" ]] ; then
-    echo "PKI_ORG must be defined"
-    exit 1
+if [[ -z "$PKI_ORG" ]]; then
+	echo "PKI_ORG must be defined"
+	exit 1
 fi
 
-if [[ -z "$PKI_ORG_UNIT" ]] ; then
-    echo "PKI_ORG_UNIT must be defined"
-    exit 1
+if [[ -z "$PKI_ORG_UNIT" ]]; then
+	echo "PKI_ORG_UNIT must be defined"
+	exit 1
 fi
 
-if [[ -z "$PKI_STATE" ]] ; then
-    echo "PKI_STATE must be defined"
-    exit 1
+if [[ -z "$PKI_STATE" ]]; then
+	echo "PKI_STATE must be defined"
+	exit 1
 fi
 
 function main() {
-    prepare
+	prepare
 
-    create_instance
+	create_instance
 
-    create_ca_csr
-    create_ca
-    create_pki_csr
-    create_certificates
+	create_ca_csr
+	create_ca
+	create_pki_csr
+	create_certificates
 
-    start_multirootca
+	start_multirootca
 }
 
 function prepare() {
-    export SUFFIX=$(tailscale dns status | awk '/MagicDNS:/ {gsub(")","") ; print $NF}')
+	export SUFFIX=$(tailscale dns status | awk '/MagicDNS:/ {gsub(")","") ; print $NF}')
 
-    if [[ -z "$SUFFIX" ]] ; then
-        echo "Error getting Tailscale's SUFFIX"
-        exit 1
-    fi
+	if [[ -z "$SUFFIX" ]]; then
+		echo "Error getting Tailscale's SUFFIX"
+		exit 1
+	fi
 
 }
 
 function create_instance() {
-    print_milestone "Deploying the PKI"
+	print_milestone "Deploying the PKI"
 
-    incus list $INSTANCE -f yaml | grep -q name: || incus launch images:debian/12 $INSTANCE
+	incus list $INSTANCE -f yaml | grep -q name: || incus launch images:debian/12 $INSTANCE
 
-    echo "echo SUFFIX=$SUFFIX | tee /etc/cloud.sh" | incus exec $INSTANCE -- bash
-    incus exec $INSTANCE -- bash < $RING0_ROOT/core-services/$INSTANCE/debian-$INSTANCE-cloud-init.sh
+	echo "echo SUFFIX=$SUFFIX | tee /etc/cloud.sh" | incus exec $INSTANCE -- bash
+	incus exec $INSTANCE -- bash <$RING0_ROOT/core-services/$INSTANCE/debian-$INSTANCE-cloud-init.sh
 
-    print_check "The PKI instance is ready to be configured"
+	print_check "The PKI instance is ready to be configured"
 }
 
 function create_ca_csr() {
-    cat << EOF > "$RING0_ROOT/core-services/pki/files/root/root-csr.json"
+	cat <<EOF >"$RING0_ROOT/core-services/pki/files/root/root-csr.json"
 {
   "CN": "Root Certificate Authority",
   "key": {
@@ -79,10 +79,10 @@ function create_ca_csr() {
   "names": [
     {
       "C": "$PKI_COUNTRY",
+      "ST": "$PKI_STATE",
       "L": "$PKI_LOCATION",
       "O": "$PKI_ORG",
-      "OU": "$PKI_ORG_UNIT",
-      "ST": "$PKI_STATE"
+      "OU": "$PKI_ORG_UNIT"
     }
   ],
   "ca": {
@@ -91,7 +91,7 @@ function create_ca_csr() {
 }
 EOF
 
-    cat << EOF > "$RING0_ROOT/core-services/pki/files/intermediate/intermediate-csr.json"
+	cat <<EOF >"$RING0_ROOT/core-services/pki/files/intermediate/intermediate-csr.json"
 {
   "CN": "Intermediate Certificate Authority",
   "key": {
@@ -101,10 +101,10 @@ EOF
   "names": [
     {
       "C": "$PKI_COUNTRY",
+      "ST": "$PKI_STATE",
       "L": "$PKI_LOCATION",
       "O": "$PKI_ORG",
-      "OU": "$PKI_ORG_UNIT",
-      "ST": "$PKI_STATE"
+      "OU": "$PKI_ORG_UNIT"
     }
   ]
 }
@@ -112,58 +112,58 @@ EOF
 }
 
 function create_ca() {
-    print_milestone "Configuring the PKI"
+	print_milestone "Configuring the PKI"
 
-    result=$(incus exec pki -- find $PKI_ROOT/files/intermediate/intermediate-fullchain.pem)
+	result=$(incus exec pki -- find $PKI_ROOT/files/intermediate/intermediate-fullchain.pem)
 
-    if [[ -z "$result" ]] ; then
-        print_milestone "Sending files to the PKI instance"
+	if [[ -z "$result" ]]; then
+		print_milestone "Sending files to the PKI instance"
 
-        if [[ ! -f "$RING0_ROOT/core-services/pki/files/root/root-csr.json" ]] ; then
-            echo "$RING0_ROOT/core-services/pki/files/root/root-csr.json must exist"
-            return 1
-        fi
+		if [[ ! -f "$RING0_ROOT/core-services/pki/files/root/root-csr.json" ]]; then
+			echo "$RING0_ROOT/core-services/pki/files/root/root-csr.json must exist"
+			return 1
+		fi
 
-        if [[ ! -f "$RING0_ROOT/core-services/pki/files/intermediate/intermediate-csr.json" ]] ; then
-            echo "$RING0_ROOT/core-services/pki/files/intermediate/intermediate-csr.json must exist"
-            return 1
-        fi
+		if [[ ! -f "$RING0_ROOT/core-services/pki/files/intermediate/intermediate-csr.json" ]]; then
+			echo "$RING0_ROOT/core-services/pki/files/intermediate/intermediate-csr.json must exist"
+			return 1
+		fi
 
-        BUFFER=$(mktemp)
-        incus exec pki -- mkdir -p $PKI_ROOT
+		BUFFER=$(mktemp)
+		incus exec pki -- mkdir -p $PKI_ROOT
 
-        cd $RING0_ROOT/core-services/pki
-        tar cvf $BUFFER .
-        cd -
+		cd $RING0_ROOT/core-services/pki
+		tar cvf $BUFFER .
+		cd -
 
-        incus file push $BUFFER pki$PKI_ROOT/pki.tar
-        echo "cd $PKI_ROOT && tar xf pki.tar" | incus exec pki -- bash
-        rm -f $BUFFER
+		incus file push $BUFFER pki$PKI_ROOT/pki.tar
+		echo "cd $PKI_ROOT && tar xf pki.tar" | incus exec pki -- bash
+		rm -f $BUFFER
 
-        print_milestone "CA fullchain"
+		print_milestone "CA fullchain"
 
-        echo "make -C $PKI_ROOT files/intermediate/intermediate-fullchain.pem" | incus exec pki -- bash
-    else
-        print_check "$result already exist"
-        print_check "The PKI has already been bootstrapped"
-    fi
+		echo "make -C $PKI_ROOT files/intermediate/intermediate-fullchain.pem" | incus exec pki -- bash
+	else
+		print_check "$result already exist"
+		print_check "The PKI has already been bootstrapped"
+	fi
 
-    print_milestone "Getting the intermediate CA"
+	print_milestone "Getting the intermediate CA"
 
-    mkdir -p dist
-    echo "cat $PKI_ROOT/files/intermediate/intermediate-fullchain.pem" | incus exec pki -- bash > dist/intermediate-fullchain.pem
-    find $RING0_ROOT/dist/intermediate-fullchain.pem
+	mkdir -p dist
+	echo "cat $PKI_ROOT/files/intermediate/intermediate-fullchain.pem" | incus exec pki -- bash >$RING0_ROOT/dist/intermediate-fullchain.pem
+	find $RING0_ROOT/dist/intermediate-fullchain.pem
 }
 
 function start_multirootca() {
-    print_milestone "Create the auth key for the webservice"
+	print_milestone "Create the auth key for the webservice"
 
-    auth_key=$(openssl rand -hex 16)
-    echo $auth_key > $RING0_ROOT/dist/auth.key
+	auth_key=$(openssl rand -hex 16)
+	echo $auth_key >$RING0_ROOT/dist/auth.key
 
-    print_milestone "Configuring multirootca"
+	print_milestone "Configuring multirootca"
 
-    cat << EOF > $RING0_ROOT/dist/config.json
+	cat <<EOF >$RING0_ROOT/dist/config.json
 {
     "signing": {
         "default": {
@@ -194,22 +194,22 @@ function start_multirootca() {
 }
 EOF
 
-    incus file push $RING0_ROOT/dist/config.json pki$PKI_ROOT/files/config/config.json
+	incus file push $RING0_ROOT/dist/config.json pki$PKI_ROOT/files/config/config.json
 
-    echo "systemctl enable multirootca.service" | incus exec $INSTANCE -- bash
-    incus restart $INSTANCE
+	echo "systemctl enable multirootca.service" | incus exec $INSTANCE -- bash
+	incus restart $INSTANCE
 
-    print_check "Checking multirootca status"
-    sleep 5
-    echo "systemctl status multirootca.service" | incus exec $INSTANCE -- bash
+	print_check "Checking multirootca status"
+	sleep 5
+	echo "systemctl status multirootca.service" | incus exec $INSTANCE -- bash
 }
 
 function create_pki_csr() {
-    print_milestone "Creating the CSR profile for $INSTANCE.$SUFFIX"
+	print_milestone "Creating the CSR profile for $INSTANCE.$SUFFIX"
 
-    hosts=$(incus list | awk "/$INSTANCE/ {print \$6}")
+	hosts=$(incus list | awk "/$INSTANCE/ {print \$6}")
 
-    cat << EOF > $RING0_ROOT/dist/$INSTANCE.$SUFFIX-csr.json
+	cat <<EOF >$RING0_ROOT/dist/$INSTANCE.$SUFFIX-csr.json
 {
   "CN": "$INSTANCE.$SUFFIX",
   "hosts": [
@@ -217,21 +217,22 @@ function create_pki_csr() {
   ],
   "names": [
     {
-      "C": "FR",
-      "L": "Paris",
-      "O": "Mushroom Cloud",
-      "OU": "CA Services"
+      "C": "$PKI_COUNTRY",
+      "ST": "$PKI_STATE",
+      "L": "$PKI_LOCATION",
+      "O": "$PKI_ORG",
+      "OU": "$PKI_ORG_UNIT"
     }
   ]
 }
 EOF
-    incus file push $RING0_ROOT/dist/$INSTANCE.$SUFFIX-csr.json pki$PKI_ROOT/files/certificates/$INSTANCE.$SUFFIX.csr.json
+	incus file push $RING0_ROOT/dist/$INSTANCE.$SUFFIX-csr.json pki$PKI_ROOT/files/certificates/$INSTANCE.$SUFFIX.csr.json
 }
 
 function create_certificates() {
-    print_milestone "Creating certificates"
+	print_milestone "Creating certificates"
 
-    echo "$PKI_ROOT/create-certificates.sh $SUFFIX" | incus exec $INSTANCE -- bash
+	echo "$PKI_ROOT/create-certificates.sh $SUFFIX" | incus exec $INSTANCE -- bash
 }
 
 main "$@"
--- ring0/scripts/init-headnode.sh
+++ ring0/scripts/init-headnode.sh
@@ -3,51 +3,51 @@
 set -e
 
 function install_tailscale() {
-    echo "#####################"
-    echo "👷 Installing Tailscale"
-    echo
+	echo "#####################"
+	echo "👷 Installing Tailscale"
+	echo
 
-    curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/plucky.noarmor.gpg \
-        | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
-    curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/plucky.tailscale-keyring.list \
-        | sudo tee /etc/apt/sources.list.d/tailscale.list
-    sudo apt update
-    sudo apt install tailscale
+	curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/plucky.noarmor.gpg |
+		sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
+	curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/plucky.tailscale-keyring.list |
+		sudo tee /etc/apt/sources.list.d/tailscale.list
+	sudo apt update
+	sudo apt install tailscale
 
-    echo "#####################"
-    echo "👷 Connecting using QR code"
-    echo
+	echo "#####################"
+	echo "👷 Connecting using QR code"
+	echo
 
-    tailscale login --qr
+	tailscale login --qr
 
-    echo
-    echo "✔ Tailscale status" 
-    echo
+	echo
+	echo "✔ Tailscale status"
+	echo
 
-    tailscale status | grep headnode
+	tailscale status | grep headnode
 
-    echo
-    echo "✔ Environment ready"
-    echo
-}	
+	echo
+	echo "✔ Environment ready"
+	echo
+}
 
 function install_incus() {
-    echo "#####################"
-    echo "👷 Installing Incus"
-    echo
+	echo "#####################"
+	echo "👷 Installing Incus"
+	echo
 
-    TAILSCALE_IPADDR=$(tailscale status | grep $HOSTNAME | awk '{print $1}')
+	TAILSCALE_IPADDR=$(tailscale status | grep $HOSTNAME | awk '{print $1}')
 
-    sudo apt -y install incus
-    sudo incus admin init
-    sudo adduser $USER incus-admin
-    newgrp incus-admin
+	sudo apt -y install incus
+	sudo incus admin init
+	sudo adduser $USER incus-admin
+	newgrp incus-admin
 
-    incus config set core.https_address $TAILSCALE_IPADDR
+	incus config set core.https_address $TAILSCALE_IPADDR
 
-    echo
-    echo "✔ Incus ready"
-    echo
+	echo
+	echo "✔ Incus ready"
+	echo
 }
 
 install_tailscale
