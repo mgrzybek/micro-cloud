6c619ffa9c33e2d518948f1cee250ed8ace783aa
Mathieu Grzybek <github@grzybek.fr>
2026-01-21 18:06:56 +0100

    ring0: add dive tool to the forge
    Signed-off-by: Mathieu Grzybek <github@grzybek.fr>

--- ring0/core-services/forge/debian-forge-cloud-init.sh
+++ ring0/core-services/forge/debian-forge-cloud-init.sh
@@ -14,7 +14,7 @@ function install_docker() {
 
 	for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
 	sudo apt-get update
-	sudo apt-get install ca-certificates curl
+	sudo apt-get install ca-certificates curl golang
 	sudo install -m 0755 -d /etc/apt/keyrings
 	sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 	sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -24,6 +24,8 @@ function install_docker() {
 		sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 	sudo apt-get update
 	sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+	go install github.com/wagoodman/dive@latest
 }
 
 main
